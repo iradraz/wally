ee92d4bc10bb0e125f0136317269c3c8a6f6b777
"jarjar" <"iradraz@gmail.com">
2019-08-31 19:50:03 +0300

    integrated both transferwise and currencycloud
    added show exchange rate page which shows brokers rates.

--- application/modules/client/views/show_exchange_rates_v.php
+++ application/modules/client/views/show_exchange_rates_v.php
@@ -1,27 +1,167 @@
-<?php
-echo '<pre>';
-print_r($first_rate);
-echo '</pre>';
-echo '<pre>';
-print_r($second_rate);
-echo '</pre>';
-?>
-<h1>price comparison for <?php echo $first_rate['client_sell_currency'] . $first_rate['client_buy_currency']; ?></h1>
-<h2>
-    CurrencyCloud rate:
-</h2>
-<h3>
-    for every <?php echo '1 ' . $first_rate['client_sell_currency']; ?>
-    you sell, you'll get <?php echo number_format($first_rate['client_buy_amount'] / $first_rate['client_sell_amount'], 4) . ' ' . $first_rate['client_buy_currency']; ?>
-    you'll receive total of: <?php echo $first_rate['client_buy_amount'] . ' ' . $first_rate['client_buy_currency']; ?>
-</h3>
-<br>
-<h2>
-    TransferWise rate:
+<style>
+    div.comparisontable{
+        display: flex;
+        flex-direction: column;
+    }
 
-</h2>
-<h3>
-    for every <?php echo '1 ' . $second_rate['source']; ?>
-    you sell, you'll get <?php echo number_format($second_rate['targetAmount'] / $second_rate['sourceAmount'], 4) . ' ' . $second_rate['target']; ?>
-    you'll receive total of: <?php echo $second_rate['targetAmount'] . ' ' . $second_rate['target']; ?>
-</h3>
+    div.comparisontable img{
+        max-width: 70%;
+        width: auto;
+        height: auto;
+    }
+
+
+    div.comparisontable ul.row{
+        list-style: none;
+        display: flex;
+        margin: 0;
+        padding: 0;
+        flex: 1;
+        width: 80%;
+        flex-wrap: wrap;
+    }
+
+    div.comparisontable ul.row li{
+        background: #c9f4ca;
+        flex: 1;
+        padding: 10px;
+        border-bottom: 1px solid gray;
+    }
+
+    div.comparisontable ul.row li.legend{
+        background: #6640d8;
+        color: white;
+        font-weight: bold;
+        border: none;
+        width: 100px;
+        border-bottom: 1px solid white;
+    }
+
+    div.comparisontable ul.row li:last-of-type{
+    }
+
+    /* very first row */
+    div.comparisontable ul.row:first-of-type li{
+        text-align: center;
+    }
+
+    /* very last row */
+    div.comparisontable ul.row:last-of-type li{
+        text-align: center;
+        border-bottom: none;
+        box-shadow: 0 6px 6px rgba(0,0,0,0.23);
+    }
+
+    div.comparisontable a.calltoaction{
+        color: white;
+        background: #e12525;
+        display: inline-block;
+        padding: 8px 10px;
+        text-decoration: none;
+        border-radius: 8px;
+        margin: .1em auto;
+    }
+
+    div.comparisontable a.calltoaction:hover{
+        background: #d11212;
+    }
+
+
+    /* first and last cell within legend column */
+    div.comparisontable ul.row:first-of-type li.legend.legend,
+    div.comparisontable ul.row:last-of-type li.legend{
+        background: transparent;
+        box-shadow: none;
+    }
+
+    @media screen and (max-width:650px){
+
+        div.comparisontable ul.row{
+            flex-direction: column;
+        }
+
+        div.comparisontable img{
+            width: auto;
+            height: auto;
+        }
+
+        div.comparisontable ul.row li{
+            margin-right: 0;
+            flex: auto;
+            width: auto;
+        }
+
+        /* first and last cell within legend column */
+        div.comparisontable ul.row:first-of-type li.legend.legend,
+        div.comparisontable ul.row:last-of-type li.legend{
+            display: none;
+        }
+
+        div.comparisontable ul.row li.legend{
+            width: auto;
+        }
+    }
+</style>
+
+<div class="comparisontable">
+    <ul class="row">
+        <li class="legend"><?php echo $second_rate['source'] . '/' . $second_rate['target']; ?> Broker Comparison</li>
+        <li><img src="<?php echo base_url('img/CurrencyCloud.png'); ?>" height="100" width="100" /><br />CurrencyCloud</li>
+        <li><img src="<?php echo base_url('img/TransferWise.png'); ?>" height="100" width="100" /><br />TransferWise</li>
+    </ul>
+
+    <ul class="row">
+        <li class="legend"><?php echo $second_rate['source']; ?> for exchange</li>
+        <li><?php echo number_format($first_rate['client_sell_amount'], 2) . ' ' . $first_rate['client_sell_currency']; ?></li>
+        <li><?php echo number_format($second_rate['sourceAmount'], 2) . ' ' . $second_rate['source']; ?></li>
+    </ul>
+
+    <ul class="row">
+        <li class="legend">Price for 1 <?php echo $second_rate['source']; ?></li>
+        <li><?php echo number_format($first_rate['client_buy_amount'] / $first_rate['client_sell_amount'], 4) . ' ' . $first_rate['client_buy_currency']; ?></li>
+        <li><?php echo number_format($second_rate['targetAmount'] / $second_rate['sourceAmount'], 4) . ' ' . $second_rate['target']; ?></li>
+    </ul>
+
+    <ul class="row">
+        <li class="legend">Broker Fee</li>
+        <li></li>
+        <li><?php echo $second_rate['fee'] . ' ' . $second_rate['target']; ?></li>
+    </ul>
+
+    <ul class="row">
+        <li class="legend"><span class="text-warning">Wally</span> Fee</li>
+        <li><?php echo $first_rate['client_buy_amount'] * 0.010 . ' ' . $first_rate['client_buy_currency']; ?></li>
+        <li><?php echo $second_rate['targetAmount'] * 0.010 . ' ' . $second_rate['target']; ?></li>
+    </ul>
+
+    <ul class="row">
+        <li class="legend">Total (Inc. fees)</li>
+        <?php
+        $currencycloudopt = ($first_rate['client_buy_amount'] - ($first_rate['client_buy_amount'] * 0.010));
+        $transferwiseopt = $second_rate['targetAmount'] - ($second_rate['targetAmount'] * 0.010) - ($second_rate['fee']);
+        ?>
+        <li><?php echo $first_rate['client_buy_amount'] - ($first_rate['client_buy_amount'] * 0.010) . ' ' . $first_rate['client_buy_currency']; ?></li>
+        <li><?php echo $second_rate['targetAmount'] - ($second_rate['targetAmount'] * 0.010) - ($second_rate['fee']) . ' ' . $second_rate['target']; ?></li>
+    </ul>
+
+    <ul class="row">
+        <li class="legend"></li>
+        <li>
+            <?php echo ($currencycloudopt < $transferwiseopt) ? '' : '<ul>you\'ll save up to ' . number_format(($currencycloudopt - $transferwiseopt),2) . ' ' . $first_rate['client_buy_currency'] . ' using CurrencyCloud</ul>' ?>
+            <ul><a href="" class="calltoaction" rel="nofollow" <?php echo ($currencycloudopt < $transferwiseopt) ? 'hidden' : ''; ?>>Exchange using CurrencyCloud</a></ul>
+        </li>
+        <li>
+            <?php echo ($currencycloudopt > $transferwiseopt) ? '' : '<ul>you\'ll save up to ' . number_format(($transferwiseopt - $currencycloudopt),2) . ' ' . $first_rate['client_buy_currency'] . ' using TransferWise</ul>' ?>
+            <a href="" class="calltoaction" rel="nofollow" <?php echo ($currencycloudopt > $transferwiseopt) ? 'hidden' : ''; ?>>Exchange using TransferWise</a>
+        </li>
+    </ul>
+</div>
+
+<?php
+//echo '<pre>';
+//print_r($first_rate);
+//echo '</pre>';
+//echo '<pre>';
+//print_r($second_rate);
+//echo '</pre>';
+?>
